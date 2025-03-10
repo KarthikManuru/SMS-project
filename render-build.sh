9189d185281447d3ea5db441b2cab85b443c1738
KarthikManuru <karthikmanuru4@gmail.com>
2025-03-10 23:08:07 +0530

    Added Rust & Cargo fix for Render deployment

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y build-essential python3-dev cargo rustc
+# Install Rust & Cargo inside Render (User Home Directory)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
+
+# Upgrade pip and install dependencies
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
